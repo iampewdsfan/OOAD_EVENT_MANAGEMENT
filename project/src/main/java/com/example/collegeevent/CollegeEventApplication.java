9fe85d7d1d6b83aedb369663ea565a6f34ab2c56
rishi s etagi <113615700+iampewdsfan@users.noreply.github.com>
2025-04-19 12:28:11 +0530

    Add files via upload

---
+++ project/src/main/java/com/example/collegeevent/CollegeEventApplication.java
@@ -0,0 +1,11 @@
+package com.example.collegeevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeEventApplication.class, args);
+    }
+}
